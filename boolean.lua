e2631692b7a0cd749f8d3dd87e55dedebfe1fdd8
oyzx <1273730703@qq.com>
2024-04-24 14:57:31 +0800

    update

---
+++ boolean.lua
@@ -0,0 +1,10 @@
+if false or nil then
+	print("至少一个是true")
+else
+	print("false 和 nil 都为false")
+end
+if 0 then
+	print("0 是true")
+else
+	print(" 0 是false")
+end
---
+++ string.lua
@@ -0,0 +1,10 @@
+html = [[
+<html>
+<head></head>
+<body>
+    <a href = "http://www.runoob.com/"> 菜鸟教程</a>
+</body>
+</html>
+]]
+
+print(html)
