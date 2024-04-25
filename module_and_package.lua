f3ab72b18c8733b1886523b8d52814e09ea56cc6
oyzx <1273730703@qq.com>
2024-04-25 16:46:31 +0800

    update

--- module_and_package.lua
+++ module_and_package.lua
@@ -8,3 +8,23 @@
 -- 加载module.lua
 local my = require("mymodule") --my是一个table,必须加上local，否则插件报错。
 print(my.constant)
+my.func3()
+
+-- require 加载机制
+
+--对于自定义模块，模块文件不能随意放在任何目录，require函数有自己的加载文件机制
+--它尝试从Lua文件或者C程序中加载模块
+
+--Lua中使用LUA_PATH初始化package.path，require在package.path中搜索模块
+--下面是我的输出：
+--[[
+/usr/local/share/lua/5.4/?.lua;
+/usr/local/share/lua/5.4/?/init.lua;
+/usr/local/lib/lua/5.4/?.lua;
+/usr/local/lib/lua/5.4/?/init.lua;
+/usr/share/lua/5.4/?.lua;/usr/share/lua/5.4/?/init.lua;
+./?.lua;
+./?/init.lua
+--]]
+
+--它们都是目录，其中？是占位符，Lua在运行的时候会将其替换成实际要加载的模块名
