c106257e3a7e440606834e25cc9ca42fe1ab8ef0
oyzx <1273730703@qq.com>
2024-04-30 15:17:11 +0800

    update

--- Lua_Basic/mymodule.lua
+++ Lua_Basic/mymodule.lua
@@ -1,6 +1,7 @@
 -- 文件名为module.lua
 -- 定义一个名为module的模块
 
+-- 空的table
 mymodule = {}
 
 --定义一个常量
@@ -24,4 +25,3 @@ return mymodule --本质上是返回table
 --func2()声明为局部变量，这是一个私有函数，不能从外部访问这个函数，必须通过模块里面的公有函数来调用。
 
 -- require函数
--- Lua使用require函数来加载模块，类似于C++的#include
